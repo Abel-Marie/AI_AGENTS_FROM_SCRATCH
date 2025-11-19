72331f3d3c1aa09d60af7802da035116fa7d35f8
Abel Marie <abelmarie49@gmail.com>
2025-11-19 11:45:30 +0300

    feat: Add initial imports from google ADK agent project. This commit introduce necessary import statements to set up the foundation of the agent project, including the agent class, Gemini model, in-memory runner, google_search tool, and custome retry configuration

---
+++ simple_adk_agent/src/agent.py
@@ -0,0 +1,6 @@
+from google.adk.agents import Agent
+from google.adk.models.google_llm import Gemini
+from google.adk.runners import InMemoryRunner
+from google.adk.tools import google_search 
+from .config import get_retry_config 
+
