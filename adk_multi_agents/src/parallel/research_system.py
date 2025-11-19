3cf1f821e3a807a2e0db9a3d36eceeba02d7d7d2
Abel Marie <abelmarie49@gmail.com>
2025-11-19 19:12:00 +0300

    feat: Start definig the research system function

--- adk_multi_agents/src/parallel/research_system.py
+++ adk_multi_agents/src/parallel/research_system.py
@@ -2,3 +2,7 @@ from google.adk.agents import Agent, ParallelAgent, SequentialAgent
 from google.adk.models.google_llm import Gemini
 from google.adk.tools import google_search
 from src.config import get_retry_config
+
+def create_research_system():
+    retry_config = get_retry_config()
+
