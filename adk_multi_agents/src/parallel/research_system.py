f7704ee3d21e391e8980e1c9b66a8f232caf08b8
Abel Marie <abelmarie49@gmail.com>
2025-11-19 19:05:44 +0300

    feat: Add imports for parallel agent creation. This commit introduce necessary imports from from the google ADK to support more complex agent workflows, including ParallelAgen, SequentialAgent, 'Agent',Gemini model, google_search tool, and custom retry configurations

---
+++ adk_multi_agents/src/parallel/research_system.py
@@ -0,0 +1,4 @@
+from google.adk.agents import Agent, ParallelAgent, SequentialAgent
+from google.adk.models.google_llm import Gemini
+from google.adk.tools import google_search
+from src.config import get_retry_config
