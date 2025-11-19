3f18f9750cf7f49fa2eb1f47a94b3d2dfd6893ba
Abel Marie <abelmarie49@gmail.com>
2025-11-19 11:54:16 +0300

    feat: Define the helpful_assistant agent configuration This commit sets up the helpful_assistant agent using the Google ADK. It initializes the agent with the Gemini 2.5 Flash Lite model, integrates the google_search tool, and applies custome retry options from the config module

--- simple_adk_agent/src/agent.py
+++ simple_adk_agent/src/agent.py
@@ -4,3 +4,17 @@ from google.adk.runners import InMemoryRunner
 from google.adk.tools import google_search 
 from .config import get_retry_config 
 
+
+def create_agent():
+    retry_config = get_retry_config()
+
+    return Agent(
+        name = "helpful_assistant",
+        model = Gemini(
+            model = "gemini-2.5-flash-lite",
+            retry_options=retry_config
+        ),
+        description="a simple agent that can answer general questions.",
+        instruction="You are a helpful assistant. Use Google Search For current info or if unsure.",
+        tools=[google_search],
+    )
