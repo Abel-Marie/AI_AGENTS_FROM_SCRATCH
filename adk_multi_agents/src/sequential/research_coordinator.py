c186c6114678335be2355233dc57a9e60ee25f48
Abel Marie <abelmarie49@gmail.com>
2025-11-19 18:10:13 +0300

    feat: Add configuration for ResearchAgent with search tool

--- adk_multi_agents/src/sequential/research_coordinator.py
+++ adk_multi_agents/src/sequential/research_coordinator.py
@@ -6,4 +6,13 @@ from src.config import get_retry_config
 def create_research_coordinator():
     retry_config = get_retry_config()
 
-    
+    # Research Agent
+    research_agent = Agent(
+        name="ResearchAgent",
+        model=Gemini(model="gemini-2.5-flash-lite", retry_options=retry_config),
+        instruction="""You are a specialized research agent. Your only job is to use the 
+        google_search tool to find 2-3 pieces of relevant information on the given topic and present the finidings with citations.""",
+        tools=[google_search],
+        output_key="research_findings",
+    )
+
