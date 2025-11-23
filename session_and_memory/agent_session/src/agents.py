ff7485df7fbc668070ebcf42fc956535ace652f2
Abel Marie <abelmarie49@gmail.com>
2025-11-23 16:41:12 +0300

    feat: add create_stateful_agent function with user context management tools

--- session_and_memory/agent_session/src/agents.py
+++ session_and_memory/agent_session/src/agents.py
@@ -4,7 +4,6 @@ from google.adk.models.google_llm import Gemini
 from .config import get_retry_config
 from .tools import save_userinfo, retrieve_userinfo
 
-
 def create_text_agent(name="text_chat_bot", description="A text chatbot"):
     """Creates a basic text chatbot agent."""
     retry_config = get_retry_config()
@@ -21,4 +20,17 @@ def create_llm_agent(name="text_chat_bot", description="A text chatbot"):
         model=Gemini(model="gemini-2.5-flash-lite", retry_options=retry_config),
         name=name,
         description=description,
+    )
+def create_stateful_agent():
+    """Creates an agent with session state tools."""
+    retry_config = get_retry_config()
+    return LlmAgent(
+        model=Gemini(model="gemini-2.5-flash-lite", retry_options=retry_config),
+        name="text_chat_bot",
+        description="""A text chatbot.
+        Tools for managing user context:
+        * To record username and country when provided use `save_userinfo` tool. 
+        * To fetch username and country when required use `retrieve_userinfo` tool.
+        """,
+        tools=[save_userinfo, retrieve_userinfo],
     )
