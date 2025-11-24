bfcbcababaf026d994328826a9b3f74a34e6ce5b
Abel Marie <abelmarie49@gmail.com>
2025-11-24 12:42:57 +0300

    feat: add create_memory_agent function to initialize a basic Gemini-powered agent

--- session_and_memory/agent_memory/src/agents.py
+++ session_and_memory/agent_memory/src/agents.py
@@ -5,3 +5,11 @@ from google.genai import types
 from .callbacks import auto_save_to_memory
 from .config import get_retry_config
 
+def create_memory_agent(name="MemoryDemoAgent"):
+    """Creates a basic agent."""
+    retry_config = get_retry_config()
+    return LlmAgent(
+        model=Gemini(model="gemini-2.5-flash-lite", retry_options=retry_config),
+        name=name,
+        instruction="Answer user questions in simple words.",
+    )
